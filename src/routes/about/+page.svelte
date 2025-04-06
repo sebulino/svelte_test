9441fbacb6d7e4a25e4aa402477c1cef91f1a3f8
Sebastian <sebastian.alscher@gmail.com>
2025-04-06 18:31:24 +0200

    CI pipeline

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>Hallo Seb!</h1>
 
 	<p>
 		This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
